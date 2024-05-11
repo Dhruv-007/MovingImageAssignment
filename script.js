99cb19b65ef1e0a6bbd1e2974101cac2537edfaa
Dhruv Johari <56172479+Dhruv-007@users.noreply.github.com>
2024-05-11 16:33:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var image = document.getElementById("movingImage");
-    var isMovingRight = true; // Flag to track direction
+    var isMovingRight = true; 
   
     document.addEventListener("click", function(event) {
       var x = event.clientX;
@@ -9,16 +9,14 @@ document.addEventListener("DOMContentLoaded", function() {
       var screenWidth = window.innerWidth;
   
       if (x <= screenWidth / 2 && isMovingRight) {
-        // If currently moving right and clicked on left, change direction
         isMovingRight = false;
         image.style.transform = "translate(-50%, -50%) scaleX(-1)";
       } else if (x > screenWidth / 2 && !isMovingRight) {
-        // If currently moving left and clicked on right, change direction
         isMovingRight = true;
         image.style.transform = "translate(-50%, -50%) scaleX(1)";
       }
   
-      image.style.transition = "left 1s, top 1s"; // Smooth transition for left and top properties
+      image.style.transition = "left 1s, top 1s"; 
       image.style.left = (x - image.offsetWidth / 2) + "px";
       image.style.top = (y - image.offsetHeight / 2) + "px";
     });
